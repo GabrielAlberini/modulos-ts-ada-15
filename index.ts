fa6172eb2a2fe8d3571eba248b8eb1bc9d26e42a
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2023-11-22 21:24:19 -0300

    Incorporación de model, controller e index

---
+++ index.ts
@@ -0,0 +1,8 @@
+import * as controller from "./controller/controller";
+
+const main = () => {
+  const data = controller.getAll();
+  const character = controller.getCharacterById(1);
+};
+
+main();
