77fd93f379665ef8cbd33188f17323d5c52cdeb5
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2023-11-22 21:29:09 -0300

    Incorporación de directorio interfaces

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ import * as controller from "./controller/controller";
 const main = () => {
   const data = controller.getAll();
   const character = controller.getCharacterById(1);
+
+  console.log(data);
 };
 
 main();
--- model/model.ts
+++ model/model.ts
@@ -1,33 +1,10 @@
-// Importando el objeto entero. Para acceder a una propiedad del objeto uso el punto.
-// import fs from "node:fs";
-// console.log(fs.readFileSync);
-
-// Destructuring, "extrayendo" una propiedad del objeto
 import { readFileSync } from "node:fs";
+import { ApiData, Character } from "../interfaces/interfaces";
 
-const PATH = "./database/data.json";
-
-interface ApiData {
-  info: any;
-  results: any;
-}
-
-interface Character {
-  id: number;
-  name: string;
-  status: string;
-  species: string;
-  type: string;
-  gender: string;
-  origin: any;
-  location: any;
-  image: string;
-  episode: string[];
-  url: string;
-  created: string;
-}
-
-const getAll = (): ApiData => JSON.parse(readFileSync(PATH).toString());
+const getAll = (): ApiData => {
+  const PATH = "./database/data.json";
+  return JSON.parse(readFileSync(PATH).toString());
+};
 
 const getCharacterById = (id: number): string => {
   const data = getAll();
